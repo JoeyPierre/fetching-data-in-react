d2f667190a95994560032ffdc8ca174877b933b5
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-10-15 14:33:27 -0400

    api connected

---
+++ src/services/weatherService.js
@@ -0,0 +1,19 @@
+
+const API_KEY = 'de52a5dea3fb4414bec175949241510';
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+
+const show = async (city) => {
+    try {
+      const queryString = `&q=${city}`;
+      const res = await fetch(BASE_URL + queryString);
+      const data = await res.json();
+      console.log('Data:', data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+ 
+show('New York');
+
+  
